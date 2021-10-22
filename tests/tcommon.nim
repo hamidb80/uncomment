fec1a462b753d885af7aebdbae35d02cc40e7b63
hamidb80 <hr.bolouri@gmail.com>
2021-10-22 21:48:54 +0330

    improve test names

--- tests/tcommon.nim
+++ tests/tcommon.nim
@@ -5,7 +5,7 @@ proc t3(res: var seq[string]){.uncomment.} =
   ## !! res.add "Yay it works"
   ## >> res.add "No ...
 
-test "e2e":
+test "common prefix":
   var stemp: seq[string]
   
   t3(stemp)
--- tests/twith.nim
+++ tests/twith.nim
@@ -1,35 +1,37 @@
 import unittest
 import uncomment
 
-proc t1(res: var seq[string]){.uncommentWith: ">> ".} =
-  ## >> res.add "first"
-  ## >> res.add "2"
-  discard 1 + 1
-  ## << res.add "NO
-  ## >> res.add "AMAZING"
-
-  if true:
-    ## >> res.add "even nested"
-
-
-proc t2(res: var seq[string]){.uncommentWith: "<< ".} =
-  ## >> res.add "NO"
+proc simple(res: var seq[string]){.uncommentWith: "<< ".} =
+  ## ++ res.add "NO"
   ## << res.add "YES"
 
+proc nested(res: var seq[string]){.uncommentWith: "<< ".} =
+  ## << res.add "first"
+  if true:
+    ## << res.add "2"
+
+  discard 1 + 1
+  if true:
+    if true:
+      ## << res.add "AMAZING"
+
+  ## >> res.add "NO
+  ## << res.add "even nested"
 
 
-test "e2e":
+test "basic":
+  var stemp: seq[string]
+  simple(stemp)
+  check stemp == ["YES"]
+
+
+test "nested":
   var stemp: seq[string]
   
-  t1(stemp)
+  nested(stemp)
   check stemp ==  [
-
     "first",
     "2",
     "AMAZING",
     "even nested",
   ]
-
-  stemp.setLen 0
-  t2(stemp)
-  check stemp == ["YES"]
