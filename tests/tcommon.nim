790b36864590b01646881dbd6a9e5a03fd9b8f87
hamidb80 <hr.bolouri@gmail.com>
2021-10-22 22:02:20 +0330

    add test

--- tests/tcommon.nim
+++ tests/tcommon.nim
@@ -2,11 +2,12 @@ import unittest
 import uncomment
 
 proc t3(res: var seq[string]){.uncomment.} =
-  ## !! res.add "Yay it works"
+  ## !! res.add "Yay"
+  ## !! res.add "it works"
   ## >> res.add "No ...
 
-test "common prefix":
+test "common prefix + mulitline":
   var stemp: seq[string]
   
   t3(stemp)
-  check stemp == ["Yay it works"]
+  check stemp == ["Yay","it works"]
